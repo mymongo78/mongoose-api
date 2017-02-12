77e331e9c20e1fdd26adf0dd7775c3ee3bbc169e
My mongo <mymongo78@gmail.com>
2017-02-12 15:24:33 +0900

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -1,33 +1,34 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
-const _ =  require('lodash');
+const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        trim:true,        
-        minlength:1,
-        unique:true,
-        validate:{
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
             validator: validator.isEmail,
             message: '{VALUE} is not a valid email'
         }
     },
-    password:{
+    password: {
         type: String,
         required: true,
         minlength: 6
     },
-    tokens:[{
-        access:{
-            type:String,
-            require:true
+    tokens: [{
+        access: {
+            type: String,
+            require: true
         },
-        token:{
-            type:String,
-            required:true
+        token: {
+            type: String,
+            required: true
         }
     }]
 });
@@ -36,15 +37,15 @@ UserSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
 
-    return _.pick(userObject,['_id','email']);
+    return _.pick(userObject, ['_id', 'email']);
 }
 
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(),access},'abc123').toString();
-    user.tokens.push({access,token});
-    return user.save().then(()=>{
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+    user.tokens.push({ access, token });
+    return user.save().then(() => {
         return token;
     });
 };
@@ -52,9 +53,9 @@ UserSchema.methods.generateAuthToken = function () {
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
-    try{
+    try {
         decoded = jwt.verify(token, 'abc123');
-    } catch (e){
+    } catch (e) {
         // return new Promise((resolve,reject)=>{
         //     reject();
         // });
@@ -62,11 +63,25 @@ UserSchema.statics.findByToken = function (token) {
     }
     return User.findOne({
         '_id': decoded._id,
-        'tokens.token':token,
-        'tokens.access':'auth'
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 }
 
-var User = mongoose.model('User',UserSchema);
+UserSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+                user.password = hash;
+                next();  
+            });
+        });              
+    } else {
+        next();
+    }
+})
 
-module.exports = {User};
+var User = mongoose.model('User', UserSchema);
+
+module.exports = { User };
