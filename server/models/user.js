16f206db27777988c73f4283e18b90a1ef972284
My mongo <mymongo78@gmail.com>
2017-02-12 20:34:25 +0900

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -68,6 +68,24 @@ UserSchema.statics.findByToken = function (token) {
     });
 }
 
+UserSchema.statics.findByCredentials = function (email,password) {
+    var User = this;
+    return User.findOne({email}).then((user)=>{
+        if (!user){
+            return Promise.reject();
+        }
+        return new Promise((resolve,reject)=>{
+            bcrypt.compare(password,user.password,(err,res)=>{
+                if (res){
+                    resolve(user);
+                } else {
+                    reject(err);
+                }
+            });
+        });        
+    });
+};
+
 UserSchema.pre('save', function (next) {
     var user = this;
     if (user.isModified('password')) {
@@ -80,7 +98,9 @@ UserSchema.pre('save', function (next) {
     } else {
         next();
     }
-})
+});
+
+
 
 var User = mongoose.model('User', UserSchema);
 
